ee81a3e4470fad5a970ca8a77d86b2cc90f53c16
Kuba Marek <blue.cube@seznam.cz>
2011-08-22 20:41:55 +0200

    The shell script may be called from anywhere, even through symlink.

--- synchronizer.sh
+++ synchronizer.sh
@@ -4,6 +4,7 @@
 # Mounts my mp3 player, synchronizes from a MPD playlist and unmounts.
 
 mount /mnt/player || exit
-./synchronizer.py --source /big/music --dest /mnt/player/MUSIC --playlist /var/lib/mpd/playlists/Player.m3u "$@"
+scriptpath=$(dirname $(readlink -f $0))
+$scriptpath/synchronizer.py --source /big/music --dest /mnt/player/MUSIC --playlist /var/lib/mpd/playlists/Player.m3u "$@"
 echo "Unmounting."
 umount /mnt/player
